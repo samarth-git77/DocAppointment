695c04265d2ae402e54b2cb0349ae6fdd451bd9a
samarth-77 <2022pgcsca088@nitjsr.ac.in>
2024-09-11 17:59:02 +0530

    http added

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -1,21 +1,16 @@
-export const generateToken = (user, message, statusCode, res) => {
-  const token = user.generateJsonWebToken();
-  // Determine the cookie name based on the user's role
-  const cookieName = user.role === 'Admin' ? 'adminToken' : 'patientToken';
-
-  res
-    .status(statusCode)
-    .cookie(cookieName, token, {
-      expires: new Date(
-        Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
-      ),
-      httpOnly: true,
-    })
-    .json({
-      success: true,
-      message,
-      user,
-      token,
-    });
-};
-
+res
+  .status(statusCode)
+  .cookie(cookieName, token, {
+    expires: new Date(
+      Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+    secure: process.env.NODE_ENV === 'production',  // Only send secure cookies in production
+    sameSite: 'strict', // Adjust this based on your cross-site needs (e.g., 'lax', 'strict', or 'none')
+  })
+  .json({
+    success: true,
+    message,
+    user,
+    token,
+  });
